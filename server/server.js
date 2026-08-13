5a6a88b71c6a29bc51516e5817b61a29e914f1be
sin <sachinbisht913@gmail.com>
2026-08-13 10:21:15 +0530

    fix: prepare server for deployment

--- server/server.js
+++ server/server.js
@@ -80,9 +80,9 @@ const startServer = async () => {
 
         connection.release();
 
-        app.listen(PORT, () => {
-            console.log(`🚀 Server running on port ${PORT}`);
-        });
+        app.listen(PORT, "0.0.0.0", () => {
+          console.log(`🚀 Server running on port ${PORT}`);
+      });
 
     } catch (error) {
 
